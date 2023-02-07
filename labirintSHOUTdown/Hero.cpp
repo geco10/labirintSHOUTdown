dc8fd2685679ce3a2b8da404032bb08443a832af
Dilena Koplande <koplande@yahoo.com>
2023-02-06 22:16:47 -0600

    tyui

--- labirintSHOUTdown/Hero.cpp
+++ labirintSHOUTdown/Hero.cpp
@@ -30,22 +30,24 @@ void Hero::setMap(Map* map){
 }
 
 void Hero::draw(sf::RenderTarget& target, sf::RenderStates states) const{
-	if (type == ShapeType::Circle) {
-		sf::CircleShape circle(map->getLen()*0.488);
-		circle.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
-		circle.setFillColor(color);
-		target.draw(circle);
-	}
-	if (type == ShapeType::Triangle) {
-		sf::CircleShape triangle(map->getLen()*0.488,3);
-		triangle.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
-		triangle.setFillColor(color);
-		target.draw(triangle);
-	}
-	if (type == ShapeType::Square) {
-		/*sf::RectangleShape square(sf::Vector2f(len));
-		square.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
-		square.setFillColor(color);
-		target.draw(square);*/
+	for (int i = 0; i < map->getSpawns().size; i++) {
+		if (type == ShapeType::Circle) {
+			sf::CircleShape circle(map->getLen() * 0.488);
+			circle.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
+			circle.setFillColor(color);
+			target.draw(circle);
+		}
+		if (type == ShapeType::Triangle) {
+			sf::CircleShape triangle(map->getLen() * 0.488, 3);
+			triangle.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
+			triangle.setFillColor(color);
+			target.draw(triangle);
+		}
+		if (type == ShapeType::Square) {
+			/*sf::RectangleShape square(sf::Vector2f(len));
+			square.setPosition(sf::Vector2f(pos.x * map->getLen(), pos.y * map->getLen()));
+			square.setFillColor(color);
+			target.draw(square);*/
+		}
 	}
 }
